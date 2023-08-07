a71b94f8241f33e52a0d9628e243854bc35f8699
luke francis <lukedevfrancis@gmail.com>
2023-08-07 14:09:16 -0400

    first commit

---
+++ intro_app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function OurText() {
+  return <p>This is my text</p>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello from React
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
