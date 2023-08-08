5486bd51d12c5bdc8a89fd9e6ef97ef85aee0bc6
luke francis <lukedevfrancis@gmail.com>
2023-08-08 17:24:06 -0400

    made it so parent event function can communicate with child

--- intro_app/src/App.js
+++ intro_app/src/App.js
@@ -4,13 +4,17 @@ import Header from './components/header';
 import Footer from './components/footer';
 
 
+function createAlert() {
+  alert('You clicked me');
+}
 
 function App() {
   return (
     <div className="App">
       <Header info='This is MY message'/>
       <p>Main Content</p>
-      <Footer trademark='page by Luke Francis'/>
+      <Footer trademark='page by Luke Francis'
+        myalert={createAlert}/>
     </div>
   );
 }
