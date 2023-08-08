8aa3696e0e521df71db6209396cdebb67bd81511
luke francis <lukedevfrancis@gmail.com>
2023-08-08 17:35:07 -0400

    worked with events in a class

--- intro_app/src/components/footer.js
+++ intro_app/src/components/footer.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
     
+    changed() {
+        console.log('something changed');
+    }
+
     render() {
-        return <h2 onClick={this.props.myalert}>
-            {this.props.trademark}
-        </h2>
+        return (
+            <div>
+                <h2 onClick={this.props.myalert}>
+                    {this.props.trademark}
+                </h2>
+                <input onChange={this.changed} type='text'/>
+            </div>
+        ) 
     }
 }
 
