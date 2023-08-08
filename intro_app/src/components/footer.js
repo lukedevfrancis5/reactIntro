d3a23fdd92a587ef0da1c450c338fe986c50f6d9
luke francis <lukedevfrancis@gmail.com>
2023-08-08 17:16:46 -0400

    worked on events

--- intro_app/src/components/footer.js
+++ intro_app/src/components/footer.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
+    
+    createAlert() {
+        alert('You clicked me');
+    }
+    
     render() {
-        return <h2>this.props.trademark</h2>
+        return <h2 onClick={this.createAlert}>
+            {this.props.trademark}
+        </h2>
     }
 }
 
