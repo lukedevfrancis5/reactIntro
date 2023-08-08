f30d5d8c1de7a4ae84e4f51ece56cfe1e36551a4
luke francis <lukedevfrancis@gmail.com>
2023-08-07 20:26:56 -0400

    worked on props

--- intro_app/src/components/footer.js
+++ intro_app/src/components/footer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
     render() {
-        return <h2> This is the Footer</h2>
+        return <h2>this.props.trademark</h2>
     }
 }
 
