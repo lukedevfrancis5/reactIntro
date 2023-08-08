47a85acebf080e9c2400bf4ad7477bb052867fb8
luke francis <lukedevfrancis@gmail.com>
2023-08-07 20:27:20 -0400

    worked on props

--- intro_app/src/components/header.js
+++ intro_app/src/components/header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Header() {
-    return <h1>This is the Header</h1>
+function Header(props) {
+    return ( 
+        <React.Fragment>
+            <h1>{props.info}</h1>
+            <h2>My number is: {props.myNumber}</h2>
+        </React.Fragment>
+    )
 }
 
 export default Header;
