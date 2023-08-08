da2aaee60e56e42dfebe34f55ff4a479543cad6c
luke francis <lukedevfrancis@gmail.com>
2023-08-08 17:24:41 -0400

    made it so child can talk to parent

--- intro_app/src/components/footer.js
+++ intro_app/src/components/footer.js
@@ -2,12 +2,8 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
     
-    createAlert() {
-        alert('You clicked me');
-    }
-    
     render() {
-        return <h2 onClick={this.createAlert}>
+        return <h2 onClick={this.props.myalert}>
             {this.props.trademark}
         </h2>
     }
